3cf3542988cce4634cd17ad803844f424b5e59dc
Arfadex <mouad.lotfi.ml@gmail.com>
2025-06-07 23:34:55 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,46 +21,26 @@ yay -S --needed --noconfirm otf-apple-fonts
 
 # Create symbolic links for configuration directories and files
 echo "Creating symbolic links..."
-ln -s ~/dotfiles/kitty ~/.config/kitty
-ln -s ~/dotfiles/starship/starship.toml ~/.config/starship.toml
-ln -s ~/dotfiles/waypaper ~/.config/waypaper
-ln -s ~/dotfiles/theming/Kvantum ~/.config/Kvantum
-ln -s ~/dotfiles/theming/nwg-look ~/.config/nwg-look
-ln -s ~/dotfiles/theming/qt6ct ~/.config/qt6ct
-ln -s ~/dotfiles/theming/.themes ~/.themes
-ln -s ~/dotfiles/theming/.icons ~/.icons
-ln -s ~/dotfiles/wal ~/.config/wal
-ln -s ~/dotfiles/wlogout ~/.config/wlogout
-ln -s ~/dotfiles/hypr ~/.config/hypr
-ln -s ~/dotfiles/catnap ~/.config/catnap
-ln -s ~/dotfiles/rofi ~/.config/rofi
-ln -s ~/dotfiles/waybar ~/.config/waybar
+ln -s ~/.config/dotfiles/kitty ~/.config/kitty
+ln -s ~/.config/dotfiles/starship/starship.toml ~/.config/starship.toml
+ln -s ~/.config/dotfiles/waypaper ~/.config/waypaper
+ln -s ~/.config/dotfiles/theming/Kvantum ~/.config/Kvantum
+ln -s ~/.config/dotfiles/theming/nwg-look ~/.config/nwg-look
+ln -s ~/.config/dotfiles/theming/qt6ct ~/.config/qt6ct
+ln -s ~/.config/dotfiles/theming/.themes ~/.themes
+ln -s ~/.config/dotfiles/theming/.icons ~/.icons
+ln -s ~/.config/dotfiles/wal ~/.config/wal
+ln -s ~/.config/dotfiles/wlogout ~/.config/wlogout
+ln -s ~/.config/dotfiles/hypr ~/.config/hypr
+ln -s ~/.config/dotfiles/catnap ~/.config/catnap
+ln -s ~/.config/dotfiles/rofi ~/.config/rofi
+ln -s ~/.config/dotfiles/waybar ~/.config/waybar
 
 mkdir -p ~/.local/share
-ln -s ~/dotfiles/applications ~/.local/share/applications
+ln -s ~/.config/dotfiles/applications ~/.local/share/applications
 
 # Copy additional configuration files
 echo "Copying configuration files..."
-cp ~/dotfiles/confs/* ~/.config/
-
-# Install Oh My Zsh and plugins
-# echo "Installing Oh My Zsh..."
-# sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-
-# echo "Installing Zsh plugins..."
-# git clone https://github.com/zsh-users/zsh-autosuggestions \
-#   ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-#   ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# # Replace default .zshrc with custom one
-# echo "Setting up Zsh configuration..."
-# rm -f ~/.zshrc
-# ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
-
-# Clone wallpapers
-echo "Cloning wallpapers..."
-git clone --depth 1 https://github.com/arfadex/wallpapers ~/Pictures/wallpapers
+cp ~/.config/dotfiles/confs/* ~/.config/
 
 echo "Installation complete!"
